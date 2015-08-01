ea01bca8222fe199f55838e25f867a962acf9636
Bryan Konowitz <bryan@kono.sh>
2015-07-31 23:18:45 -0700

    first pass of symlinking in kitchenplan

--- recipes/kitchenplan.rb
+++ recipes/kitchenplan.rb
@@ -5,4 +5,18 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 include_recipe "zgen::default"
+::Chef::Recipe.send(:include, Zgen::Helpers)
 
+zgen_users.each do |user|
+  self.active_user = user
+
+  Dir.glob("#{zgen_home}/**/*.symlink").each do |sym|
+    name = sym.split('/').last.chomp('.symlink').prepend('.')
+    target_file = "#{user_home}/#{name}"
+    unless File.file? target_file
+      link target_file do
+        to sym
+      end
+    end
+  end
+end
